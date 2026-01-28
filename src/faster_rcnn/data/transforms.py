88a3b69a9a0e6a48d6e950ffd99a77ad3f809fa8
codersgl <sgl205537@outlook.com>
2026-01-28 21:02:49 +0800

    Return torch.Tensor and fix resizing

--- src/faster_rcnn/data/transforms.py
+++ src/faster_rcnn/data/transforms.py
@@ -1,5 +1,6 @@
 from typing import Callable, Tuple
 
+import torch
 from PIL import Image
 from torchvision import transforms
 
@@ -7,7 +8,7 @@ from torchvision import transforms
 def get_transforms(
     mean: Tuple[float, float, float] = (0.485, 0.456, 0.406),
     std: Tuple[float, float, float] = (0.229, 0.224, 0.225),
-) -> Callable[[Image.Image], Image.Image]:
+) -> Callable[[Image.Image], torch.Tensor]:
     """Get transforms for image preprocessing.
 
     Args:
@@ -15,18 +16,29 @@ def get_transforms(
         std (Tuple[float, float, float]): Standard deviation values for normalization.
 
     Returns:
-        Callable[[Image.Image], Image.Image]: A callable that applies the transforms to an image.
+        Callable[[Image.Image], torch.Tensor]: A callable that applies the transforms to an image.
     """
 
-    # Image are resized so that the shorter side is 600 pixels long
     def resize_shorter_side(image: Image.Image) -> Image.Image:
-        if image.width > image.height:
-            return image.resize((int(image.width * 600 / image.height), 600))
-        else:
-            return image.resize((600, int(image.height * 600 / image.width)))
+        """Resize image so that the shorter side is 600 pixels long."""
+        width, height = image.size
 
-    return transforms.Compose([
-        resize_shorter_side,
-        transforms.ToTensor(),
-        transforms.Normalize(mean=mean, std=std),
-    ])
+        if width < height:
+            new_width = 600
+            new_height = int(height * 600 / width)
+        else:
+            new_height = 600
+            new_width = int(width * 600 / height)
+
+        new_width = int(new_width)
+        new_height = int(new_height)
+
+        return image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+    return transforms.Compose(
+        [
+            resize_shorter_side,
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean, std=std),
+        ]
+    )
